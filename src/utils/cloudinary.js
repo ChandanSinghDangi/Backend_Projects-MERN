3547592cf8d87ace2b22a5b740a1458cd2401e7b
ChandanSinghDangi <chandandangi429@gmail.com>
2025-09-29 18:30:44 +0530

    config multer, and using cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from 'fs'
+
+
+const uploadOnCloudinary = async(localFilePath) => {
+    try {
+
+        if(!localFilePath) return null;
+
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        });
+
+        console.log('File Upload on Cloudinary was Successfull :: ',response.url); // check the result and see what it gives...
+
+        if(response) {
+            fs.unlinkSync(localFilePath);
+            return response; // give full response to user for now...
+        }
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+        return null
+    }
+}
+
+export { uploadOnCloudinary }
